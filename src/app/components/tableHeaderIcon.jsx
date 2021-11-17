72aba32c22466bee58cdb3b2456d4bb1f2633f1d
Пользователь <Rogvittt@mail.ru>
2021-11-17 23:20:37 +0300

    homework sort

---
+++ src/app/components/tableHeaderIcon.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TableHeaderIcon = ({ selectedSort, columnPath }) => {
+    if (columnPath && columnPath === selectedSort.path) {
+        return (
+            <i
+                className={
+                    "bi bi-caret-" +
+                    (selectedSort.order === "asc" ? "down" : "up") +
+                    "-fill "
+                }
+            ></i>
+        );
+    }
+    return null;
+};
+
+TableHeaderIcon.propTypes = {
+    selectedSort: PropTypes.object.isRequired,
+    columnPath: PropTypes.string
+};
+
+export default TableHeaderIcon;
